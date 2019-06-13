b0c3a551592de12d22329b04c9fffd3b21d1cb5a
Tony Blacker <tblacker7@gmail.com>
2019-06-13 14:35:36 +0100

    added plugin dependancies and forced installation

--- functions.php
+++ functions.php
@@ -293,6 +293,68 @@ function gutenberg_sections_register_acf_color_palette() {
 }
 
 /**
+ * Force download of dependancy plugins
+ */
+require_once get_template_directory() . '/lib/class-tgm-plugin-activation.php';
+
+add_action( 'tgmpa_register', 'nightingale_2_0_register_required_plugins' );
+function nightingale_2_0_register_required_plugins() {
+    /*
+     * Array of plugin arrays. Required keys are name and slug.
+     * If the source is NOT from the .org repo, then source is also required.
+     */
+    $plugins = array(
+
+        // This is an example of how to include a plugin bundled with a theme.
+        array(
+            'name'               => 'NHS Leadership Academy Blocks for Gutenberg', // The plugin name.
+            'slug'               => 'nhsl-blocks', // The plugin slug (typically the folder name).
+            'source'             => get_template_directory() . '/lib/plugins/nhsl-blocks.zip', // The plugin source.
+            'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+            'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+            'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be
+            // deactivated until theme switch.
+            'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+            'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+            'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+        ),
+
+        // This is an example of how to include a plugin from an arbitrary external source in your theme.
+        array(
+            'name'         => 'Advanced Custom Fields PRO', // The plugin name.
+            'slug'         => 'advanced-custom-fields-pro', // The plugin slug (typically the folder name).
+            'source'       => get_template_directory() . '/lib/plugins/advanced-custom-fields-pro.zip', // The plugin
+            // source.
+            'required'     => true, // If false, the plugin is only 'recommended' instead of required.
+            'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be
+            // deactivated until theme switch.
+            'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+            'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+            'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+        ),
+
+    );
+
+    /*
+     * Array of configuration settings. Amend each line as needed.
+     */
+    $config = array(
+        'id'           => 'NHS',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+        'default_path' => '',                      // Default absolute path to bundled plugins.
+        'menu'         => 'tgmpa-install-plugins', // Menu slug.
+        'has_notices'  => true,                    // Show admin notices or not.
+        'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+        'dismiss_msg'  => 'You must install the required plugins for this theme to function',                      //
+        // If 'dismissable' is false, this message will be output at top of nag.
+        'is_automatic' => true,                   // Automatically activate plugins after installation or not.
+        'message'      => '',                      // Message to output right before the plugins table.
+
+    );
+
+    tgmpa( $plugins, $config );
+}
+
+/**
  * Enqueue Gutenberg block editor style
  */
 function nhsl_gutenberg_editor_styles() {
