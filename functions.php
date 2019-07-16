99acfb61f9b359ed19a7db95cdc854adab1bb9d8
Tony Blacker <tblacker7@gmail.com>
2019-07-16 15:06:05 +0100

    Added theme defaults:
     - on first install the top and footer menus are created and attached correctly
     - - both start with just a home link.
     - moved BE Subpages widget inside the theme
     - - renamed all be_ classes to nightingale_2_0_ to avoid conflict
     - - enabled auto deployment of widget to sidebar widget area

--- functions.php
+++ functions.php
@@ -8,41 +8,41 @@
  */
 
 if ( ! function_exists( 'nightingale_2_0_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
-	function nightingale_2_0_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on Nightingale 2.0, use a find and replace
-		 * to change 'nightingale-2-0' to the name of your theme in all the template files.
-		 */
-		load_theme_textdomain( 'nightingale-2-0', get_template_directory() . '/languages' );
+    /**
+     * Sets up theme defaults and registers support for various WordPress features.
+     *
+     * Note that this function is hooked into the after_setup_theme hook, which
+     * runs before the init hook. The init hook is too late for some features, such
+     * as indicating support for post thumbnails.
+     */
+    function nightingale_2_0_setup() {
+        /*
+         * Make theme available for translation.
+         * Translations can be filed in the /languages/ directory.
+         * If you're building a theme based on Nightingale 2.0, use a find and replace
+         * to change 'nightingale-2-0' to the name of your theme in all the template files.
+         */
+        load_theme_textdomain( 'nightingale-2-0', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+        // Add default posts and comments RSS feed links to head.
+        add_theme_support( 'automatic-feed-links' );
 
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
-		add_theme_support( 'title-tag' );
+        /*
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
+        add_theme_support( 'title-tag' );
 
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
-		add_theme_support( 'post-thumbnails' );
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support( 'post-thumbnails' );
 
-		// This theme uses wp_nav_menu() in one location.
+        // This theme uses wp_nav_menu() in one location.
         function register_my_menus() {
             register_nav_menus(
                 array(
@@ -53,39 +53,76 @@ if ( ! function_exists( 'nightingale_2_0_setup' ) ) :
         }
         add_action( 'init', 'register_my_menus' );
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
+        /*
+         * Switch default core markup for search form, comment form, and comments
+         * to output valid HTML5.
+         */
+        add_theme_support( 'html5', array(
+            'search-form',
+            'comment-form',
+            'comment-list',
+            'gallery',
+            'caption',
+        ) );
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support( 'custom-background', apply_filters( 'nightingale_2_0_custom_background_args', array(
-			'default-color' => 'ffffff',
-			'default-image' => '',
-		) ) );
+        // Set up the WordPress core custom background feature.
+        add_theme_support( 'custom-background', apply_filters( 'nightingale_2_0_custom_background_args', array(
+            'default-color' => 'ffffff',
+            'default-image' => '',
+        ) ) );
 
-		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+        // Add theme support for selective refresh for widgets.
+        add_theme_support( 'customize-selective-refresh-widgets' );
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
-		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
-			'flex-width'  => true,
-			'flex-height' => true,
-		) );
-	}
+        /**
+         * Add support for core custom logo.
+         *
+         * @link https://codex.wordpress.org/Theme_Logo
+         */
+        add_theme_support( 'custom-logo', array(
+            'height'      => 250,
+            'width'       => 250,
+            'flex-width'  => true,
+            'flex-height' => true,
+        ) );
+        // Load regular editor styles into the new block-based editor.
+        add_theme_support( 'editor-styles' );
+        // Load default block styles.
+        add_theme_support( 'wp-block-styles' );
+        // Add support for responsive embeds.
+        add_theme_support( 'responsive-embeds' );
+        // Define and register starter content to showcase the theme on new sites.
+        $starter_content = array(
+            'widgets'     => array(
+                // Place three core-defined widgets in the sidebar area.
+                'sidebar-1' => array(
+                    'Nightingale_2_0_Subpages_Widget',
+                ),
+            ),
+
+            // Set up nav menus for each of the two areas registered in the theme.
+            'nav_menus'   => array(
+                // Assign a menu to the "main-menu" location.
+                'main-menu'    => array(
+                    'name'  => __( 'menu-top-menu', 'nightingale_2_0' ),
+                    'items' => array(
+                        'link_home', // Note that the core "home" page is actually a link in case a static front page is not used.
+                    ),
+                ),
+                // Assign a menu to the "footer-menu" location.
+                'footer-menu' => array(
+                    'name'  => __( 'Footer Links', 'nightingale_2_0' ),
+                    'items' => array(
+                        'link_home',
+                    ),
+                ),
+            ),
+        );
+
+        $starter_content = apply_filters( 'nightingale_2_0_starter_content', $starter_content );
+        add_theme_support( 'starter-content', $starter_content );
+
+    }
 endif;
 add_action( 'after_setup_theme', 'nightingale_2_0_setup' );
 
@@ -97,10 +134,10 @@ add_action( 'after_setup_theme', 'nightingale_2_0_setup' );
  * @global int $content_width
  */
 function nightingale_2_0_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'nightingale_2_0_content_width', 640 );
+    // This variable is intended to be overruled from themes.
+    // Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
+    // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
+    $GLOBALS['content_width'] = apply_filters( 'nightingale_2_0_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'nightingale_2_0_content_width', 0 );
 
@@ -110,15 +147,15 @@ add_action( 'after_setup_theme', 'nightingale_2_0_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function nightingale_2_0_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'nightingale-2-0' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'nightingale-2-0' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+    register_sidebar( array(
+        'name'          => esc_html__( 'Sidebar', 'nightingale-2-0' ),
+        'id'            => 'sidebar-1',
+        'description'   => esc_html__( 'Add widgets here.', 'nightingale-2-0' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
 }
 add_action( 'widgets_init', 'nightingale_2_0_widgets_init' );
 
@@ -126,15 +163,15 @@ add_action( 'widgets_init', 'nightingale_2_0_widgets_init' );
  * Enqueue scripts and styles.
  */
 function nightingale_2_0_scripts() {
-	wp_enqueue_style( 'nightingale-2-0-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'nightingale-2-0-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'nightingale-2-0-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+    wp_enqueue_script( 'nightingale-2-0-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'nightingale-2-0-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    wp_enqueue_script( 'nightingale-2-0-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
 }
 add_action( 'wp_enqueue_scripts', 'nightingale_2_0_scripts' );
 
@@ -400,6 +437,11 @@ if ( defined( 'JETPACK__VERSION' ) ) {
     require get_template_directory() . '/inc/jetpack.php';
 
 }
+/**
+ * Auto deploy subpages widget.
+ */
+require get_template_directory() . '/inc/nightingale_subpages_widget.php';
+
 /**
  * Disable XML RPC by default
  */
