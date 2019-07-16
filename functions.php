f430390a62b8109631f8c1fd055b5a9e712256bd
Tony Blacker <tblacker7@gmail.com>
2019-07-16 13:39:23 +0100

    Theme modifications:
     - removed featured image from single page/post view. Featured image shows only in lists of posts/pages
     - dashboard look and feel modified (smaller text, slightly wider banner)
     - pixel_trans modified to get better ration of dashboard blocks

--- functions.php
+++ functions.php
@@ -400,6 +400,7 @@ if ( defined( 'JETPACK__VERSION' ) ) {
     require get_template_directory() . '/inc/jetpack.php';
 
 }
+
 /**
  * Disable XML RPC by default
  */
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -14,7 +14,7 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<?php nightingale_2_0_post_thumbnail(); ?>
+
 
 	<div class="entry-content">
 		<?php
