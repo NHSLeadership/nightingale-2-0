93701cb63c485b2c117eb58e36446e180e403798
Tony Blacker <tblacker7@gmail.com>
2019-08-19 12:22:00 +0100

    update to bring into line with local development work

--- functions.php
+++ functions.php
@@ -230,6 +230,19 @@ function nightingale_2_0_register_required_plugins() {
             'external_url'       => '', // If set, overrides default API URL and points to an external URL.
             'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
         ),
+        // Load in Gutenberg plugin directly from WP repo.
+        array(
+            'name'               => 'Gutenberg', // The plugin name.
+            'slug'               => 'gutenberg', // The plugin slug (typically the folder name).
+            'source'             => '', // The plugin source.
+            'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+            'version'            => '6.3.0', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+            'force_activation'   => true, // If true, plugin is activated upon theme activation and cannot be
+            // deactivated until theme switch.
+            'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+            'external_url'       => '', // If set, overrides default API URL and points to an external URL.
+            'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
+        ),
 
 
 
