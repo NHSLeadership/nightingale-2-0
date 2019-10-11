9dd8e2b0712d05f356d4861aec6e3fecfd70d825
Tony Blacker <tblacker7@gmail.com>
2019-10-11 16:05:53 +0100

    Comments styling:
     - Turned comments to flex layout
     - put slightly radial bordered background behind comment text
     - floated user name to left
     - removed time of comment, left date
     - put avatar to right of name, left of actual comment and radius 50% to display as circle
     - added reply and edit icons and floated to right with text labels for accessibility
     - added alternative view flex-wrap on less than 800px layout for reponsive view
     - reduced indentation of replies to allow greater text area

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -157,30 +157,32 @@ function nightingale_comment_display($comment, $args, $depth) {
         <div id="div-comment-<?php comment_ID() ?>" class="comment-body"><?php
 	} ?>
 
-	<?php comment_text(); ?>
-    <div class="comment-author vcard"><?php
-	if ( $args['avatar_size'] != 0 ) {
+    <div class="comment_body_comment"><?php comment_text(); ?></div>
+    <div class="comment-avatar"><?php if ( $args['avatar_size'] != 0 ) {
 		echo get_avatar( $comment, $args['avatar_size'] );
 	}
+        ?></div>
+    <div class="comment-author vcard"><?php
+
 	printf( __( '<cite class="fn">%s</cite>' ), get_comment_author_link() ); ?>
-    </div><?php
-	if ( $comment->comment_approved == '0' ) { ?>
-        <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em><br/><?php
-	} ?>
+
     <div class="comment-meta commentmetadata">
         <a href="<?php echo htmlspecialchars( get_comment_link( $comment->comment_ID ) ); ?>"><?php
 			/* translators: 1: date, 2: time */
 			printf(
-				__('%1$s at %2$s'),
-				get_comment_date(),
-				get_comment_time()
+				__('%1$s'),
+				get_comment_date()
 			); ?>
-        </a><?php
-		edit_comment_link( __( '(Edit)' ), '  ', '' ); ?>
+        </a>
     </div>
+    </div><?php
+	if ( $comment->comment_approved == '0' ) { ?>
+        <em class="comment-awaiting-moderation"><?php _e( 'Your comment is awaiting moderation.' ); ?></em><br/><?php
+	} ?>
 
 
-    <div class="reply"><?php
+    <div class="reply">
+    <?php
 	comment_reply_link(
 		array_merge(
 			$args,
@@ -191,9 +193,26 @@ function nightingale_comment_display($comment, $args, $depth) {
 			)
 		)
 	); ?>
+	<?php
+	edit_comment_link( __( nightingale_edit_icon() ), '  ', '' ); ?>
     </div><?php
 	if ( 'div' != $args['style'] ) : ?>
         </div><?php
 	endif;
 }
 
+function nightingale_reply_icon () {
+    return '<svg class="comment-reply" viewBox="0 0 20 20">
+							<path d="M14.999,8.543c0,0.229-0.188,0.417-0.416,0.417H5.417C5.187,8.959,5,8.772,5,8.543s0.188-0.417,0.417-0.417h9.167C14.812,8.126,14.999,8.314,14.999,8.543 M12.037,10.213H5.417C5.187,10.213,5,10.4,5,10.63c0,0.229,0.188,0.416,0.417,0.416h6.621c0.229,0,0.416-0.188,0.416-0.416C12.453,10.4,12.266,10.213,12.037,10.213 M14.583,6.046H5.417C5.187,6.046,5,6.233,5,6.463c0,0.229,0.188,0.417,0.417,0.417h9.167c0.229,0,0.416-0.188,0.416-0.417C14.999,6.233,14.812,6.046,14.583,6.046 M17.916,3.542v10c0,0.229-0.188,0.417-0.417,0.417H9.373l-2.829,2.796c-0.117,0.116-0.71,0.297-0.71-0.296v-2.5H2.5c-0.229,0-0.417-0.188-0.417-0.417v-10c0-0.229,0.188-0.417,0.417-0.417h15C17.729,3.126,17.916,3.313,17.916,3.542 M17.083,3.959H2.917v9.167H6.25c0.229,0,0.417,0.187,0.417,0.416v1.919l2.242-2.215c0.079-0.077,0.184-0.12,0.294-0.12h7.881V3.959z"></path>
+						</svg> Reply';
+}
+function nightingale_edit_icon () {
+    return '<svg class="comment-edit" viewBox="0 0 20 20">
+							<path d="M19.404,6.65l-5.998-5.996c-0.292-0.292-0.765-0.292-1.056,0l-2.22,2.22l-8.311,8.313l-0.003,0.001v0.003l-0.161,0.161c-0.114,0.112-0.187,0.258-0.21,0.417l-1.059,7.051c-0.035,0.233,0.044,0.47,0.21,0.639c0.143,0.14,0.333,0.219,0.528,0.219c0.038,0,0.073-0.003,0.111-0.009l7.054-1.055c0.158-0.025,0.306-0.098,0.417-0.211l8.478-8.476l2.22-2.22C19.695,7.414,19.695,6.941,19.404,6.65z M8.341,16.656l-0.989-0.99l7.258-7.258l0.989,0.99L8.341,16.656z M2.332,15.919l0.411-2.748l4.143,4.143l-2.748,0.41L2.332,15.919z M13.554,7.351L6.296,14.61l-0.849-0.848l7.259-7.258l0.423,0.424L13.554,7.351zM10.658,4.457l0.992,0.99l-7.259,7.258L3.4,11.715L10.658,4.457z M16.656,8.342l-1.517-1.517V6.823h-0.003l-0.951-0.951l-2.471-2.471l1.164-1.164l4.942,4.94L16.656,8.342z"></path>
+						</svg> Edit';
+}
+function nightingale_comment_reply_text( $link ) {
+	$link = str_replace( 'Reply', nightingale_reply_icon(), $link );
+	return $link;
+}
+add_filter( 'comment_reply_link', 'nightingale_comment_reply_text' );
