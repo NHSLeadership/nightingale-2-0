e38a0d7634641c473546b8cdd9107f0ecb613e3e
Tony Blacker <tblacker7@gmail.com>
2020-01-09 15:09:28 +0000

    Search form and default fonts:
     - Added counter to searchform.php to determine if more than one form is displayed ona  page. If it is, modify the ids so there are no more duplicates in html
     - Removed id's completely in second and subsequent forms relating to autosuggest, collapsible fields etc as not required
     - modified css to ensure search form displays correctly when no header search field is set in customiser
     - modified default font in layouts/default.scss as for some reason this took precendence. Now defined as Frutiger by default

--- searchform.php
+++ searchform.php
@@ -12,30 +12,51 @@ if ( ! empty( get_search_query() ) ) {
 } else {
 	$query = 'Search';
 }
+
+if ( ! isset( $GLOBALS['nightingale_search_form_counter'] ) ) {
+	$GLOBALS['nightingale_search_form_counter'] = 1;
+	$searchid                                   = '';
+	$toggle_search = 'id="toggle-search"';
+	$wrap_search = 'id="wrap-search"';
+	$search_form = 'id="search"';
+	$search_field = 'search-field';
+	$autocomplete = 'id="autocomplete-container"';
+	$close_search = 'id="close-search"';
+} else {
+	$GLOBALS['nightingale_search_form_counter'] ++;
+	$searchid = $GLOBALS['nightingale_search_form_counter'];
+	$toggle_search = '';
+	$wrap_search = '';
+	$autocomplete = '';
+	$close_search = '';
+	$search_form = 'id="search' . $searchid . '"';
+	$search_field = 'search-field' . $searchid;
+}
+
 ?>
-<button class="nhsuk-header__search-toggle" id="toggle-search" aria-controls="search" aria-label="Open search">
-	<svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-		<path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path>
-	</svg>
-	<span class="nhsuk-u-visually-hidden">Search</span>
+<button class="nhsuk-header__search-toggle" <?php echo $toggle_search; ?> aria-controls="search" aria-label="Open search">
+    <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+        <path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path>
+    </svg>
+    <span class="nhsuk-u-visually-hidden">Search</span>
 </button>
-<div class="nhsuk-header__search-wrap" id="wrap-search">
-	<form class="nhsuk-header__search-form" id="search" action="/" method="get" role="search">
-		<label class="nhsuk-u-visually-hidden" for="search-field">Search the NHS website</label>
-		<div class="autocomplete-container" id="autocomplete-container"></div>
-		<input class="nhsuk-search__input" id="search-field" name="s" type="search" placeholder="Search" autocomplete="off">
-		<button class="nhsuk-search__submit" type="submit">
-			<svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-				<path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path>
-			</svg>
-			<span class="nhsuk-u-visually-hidden">Search</span>
-		</button>
-		<button class="nhsuk-search__close" id="close-search">
-			<svg class="nhsuk-icon nhsuk-icon__close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
-				<path d="M13.41 12l5.3-5.29a1 1 0 1 0-1.42-1.42L12 10.59l-5.29-5.3a1 1 0 0 0-1.42 1.42l5.3 5.29-5.3 5.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l5.29-5.3 5.29 5.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z"></path>
-			</svg>
-			<span class="nhsuk-u-visually-hidden">Close search</span>
-		</button>
-	</form>
+<div class="nhsuk-header__search-wrap" <?php echo $wrap_search; ?>>
+    <form class="nhsuk-header__search-form" <?php echo $search_form; ?> action="/" method="get" role="search">
+        <label class="nhsuk-u-visually-hidden" for="<?php echo $search_field; ?>">Search the NHS website</label>
+        <div class="autocomplete-container" <?php echo $autocomplete; ?>></div>
+        <input class="nhsuk-search__input" id="<?php echo $search_field; ?>" name="s" type="search" placeholder="Search" autocomplete="off">
+        <button class="nhsuk-search__submit" type="submit">
+            <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                <path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path>
+            </svg>
+            <span class="nhsuk-u-visually-hidden">Search</span>
+        </button>
+        <button class="nhsuk-search__close" <?php echo $close_search; ?>>
+            <svg class="nhsuk-icon nhsuk-icon__close" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
+                <path d="M13.41 12l5.3-5.29a1 1 0 1 0-1.42-1.42L12 10.59l-5.29-5.3a1 1 0 0 0-1.42 1.42l5.3 5.29-5.3 5.29a1 1 0 0 0 0 1.42 1 1 0 0 0 1.42 0l5.29-5.3 5.29 5.3a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42z"></path>
+            </svg>
+            <span class="nhsuk-u-visually-hidden">Close search</span>
+        </button>
+    </form>
 </div>
 
