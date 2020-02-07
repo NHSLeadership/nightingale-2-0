d38a313fdf63c2d4621bdf09b1b183b2e34c3c3d
Belinda Mustoe <belinda@verytwisty.com>
2020-02-07 12:09:27 +0000

    Trying to get overrides to work plus making sure events calendar plugin is loaded

--- inc/events-calendar.php
+++ inc/events-calendar.php
@@ -132,3 +132,59 @@ function nightingale_events_icons(){
 		'wallet' => '<svg class="nhsuk-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M448 112V96c0-35.35-28.65-64-64-64H96C42.98 32 0 74.98 0 128v256c0 53.02 42.98 96 96 96h352c35.35 0 64-28.65 64-64V176c0-35.35-28.65-64-64-64zm16 304c0 8.82-7.18 16-16 16H96c-26.47 0-48-21.53-48-48V128c0-26.47 21.53-48 48-48h288c8.82 0 16 7.18 16 16v32H112c-8.84 0-16 7.16-16 16s7.16 16 16 16h336c8.82 0 16 7.18 16 16v240zm-80-160c-17.67 0-32 14.33-32 32s14.33 32 32 32 32-14.33 32-32-14.33-32-32-32z"/></svg>'
 	);
 }
+
+
+
+/**
+ * Redirect event category requests to list view.
+ *
+ * @param $query
+ */
+function use_list_view_for_categories( $query ) {
+	// Disregard anything except a main archive query
+	if ( is_admin() || ! $query->is_main_query() || ! is_archive() ) return;
+
+	// We only want to catch *event* category requests being issued
+	// against something other than list view
+	if ( ! $query->get( 'tribe_events_cat' ) ) return;
+	if ( tribe_is_list_view() ) return;
+
+	// Get the term object
+	$term = get_term_by( 'slug', $query->get( 'tribe_events_cat' ), Tribe__Events__Main::TAXONOMY );
+
+	// If it's invalid don't go any further
+	if ( ! $term ) return;
+
+	// Get the list-view taxonomy link and redirect to it
+	header( 'Location: ' . tribe_get_listview_link( $term->term_id ) );
+	exit();
+}
+
+// Use list view for category requests by hooking into pre_get_posts for event queries
+// add_action( 'tribe_events_pre_get_posts', 'use_list_view_for_categories' );
+
+// To do keep list context
+
+function add_list_view( $view_context, $view_slug, $instance ){
+	
+
+	return $view_context;
+
+}
+
+// add_filter( 'tribe_events_views_v2_view_context', 'add_list_view', 15, 3 );
+
+
+/**
+ * Disable Gutenberg for events pages even if they have checked the enable btn
+ */
+
+// To do disable Gutenberg
+
+function disable_gutenberg(){
+	return true;
+}
+
+add_filter( 'tribe_is_classic_editor_plugin_active', 'disable_gutenberg' );
+
+
