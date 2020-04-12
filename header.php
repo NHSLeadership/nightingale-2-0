589bc5ec5fca86a93fb576172883e883485e4859
Tony Blacker <tblacker7@gmail.com>
2020-04-12 19:34:31 +0100

    Theme Review Third Attempt....
     - readmore function - pretty certain this has now been completely removed, it was only returning anything for admin and that was undefined. Left function in place but return null
     - header now uses wp_open_body function (function existed but wasnt being called)
     - re-added textdomain declaration so that future i18n can happen.
     - nhsuk.js de-minified

--- header.php
+++ header.php
@@ -32,6 +32,7 @@ if ( ! function_exists( 'wp_body_open' ) ) {
 		do_action( 'wp_body_open' );
 	}
 }
+wp_body_open();
 ?>
 <?php do_action( 'nightingale_after_body' ); ?>
 <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'nightingale' ); ?></a>
