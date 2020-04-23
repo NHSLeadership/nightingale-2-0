50210322cc6b6497ef23a5c9e3286cc9ad0e1c4b
Tony Blacker <tblacker7@gmail.com>
2020-04-23 16:04:47 +0100

    bumped release number to 2.1.5

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  * @link      https://developer.wordpress.org/themes/basics/theme-functions/
  * @package   Nightingale
  * @copyright NHS Leadership Academy, Tony Blacker
- * @version   2.1.0 19th November 2019
+ * @version   2.1.5 23rd April 2020
  */
 
 /**
