1cb63c191dd19cb567a28740d266463037dbc0d0
Chris Witham <cehwitham@gmail.com>
2020-07-21 15:39:25 +0100

    Include new max-width class on primary div

--- 404.php
+++ 404.php
@@ -16,7 +16,7 @@
 
 get_header();
 ?>
-<div id="primary" class=" nhsuk-grid-row">
+<div id="primary" class=" nhsuk-grid-row nhsuk-width-restrict">
 
 	<div class="nhsuk-grid-column-full full-width">
 		<section class="error-404 not-found">
