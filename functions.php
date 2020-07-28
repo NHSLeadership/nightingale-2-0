4830e9548b87bb1ecc69c8bbda12fadb8c068187
Tony Blacker <tblacker7@gmail.com>
2020-07-28 17:27:18 +0100

    properly align stuff on all resolutions

--- functions.php
+++ functions.php
@@ -243,7 +243,7 @@ function nightingale_scripts() {
 	wp_enqueue_style( 'nightingale-page-colours', get_template_directory_uri() . '/page-colours.min.css', array(), '20202704' );
 	wp_enqueue_script( 'nightingale-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', '', '20190828', true );
 	wp_enqueue_script( 'nightingale-nhs-library', get_template_directory_uri() . '/js/nhsuk.min.js', '', '20190828', true );
-
+	wp_enqueue_script( 'nightingale-navigation', get_template_directory_uri() . '/js/navigation.js', '', '20190828', true );
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
