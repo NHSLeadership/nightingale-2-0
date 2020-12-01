02287e904f827c4edc5b3d6e4eef0204eac9f879
Tony Blacker <tblacker7@gmail.com>
2020-12-01 11:48:05 +0000

    Based on work from @tporteus

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,32 +10,36 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+    <header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+    </header><!-- .entry-header -->
 
 	<?php do_action( 'nightingale_before_single_content' ); ?>
 
-	<div class="entry-content">
+    <div class="entry-content">
 		<?php
-		if ( function_exists( 'nightingale_clean_bad_content' ) ) {
-			nightingale_clean_bad_content( true );
+		if ( ! post_password_required() ) {
+			if ( function_exists( 'nightingale_clean_bad_content' ) ) {
+				nightingale_clean_bad_content( true );
+			}
+		} else {
+			echo get_the_password_form( $post->ID );
 		}
 		?>
 
 		<?php do_action( 'page_after_content' ); ?>
-	</div><!-- .entry-content -->
-	<div class="nhsuk-content__clearfix"></div>
+    </div><!-- .entry-content -->
+    <div class="nhsuk-content__clearfix"></div>
 
 	<?php do_action( 'nightingale_after_single_content' ); ?>
 
 	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
+        <footer class="entry-footer">
 			<?php
 			edit_post_link(
 				sprintf(
 					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
+					/* translators: %s: Name of current post. Only visible to screen readers */
 						__( 'Edit <span class="screen-reader-text">%s</span>', 'nightingale' ),
 						array(
 							'span' => array(
@@ -49,6 +53,6 @@
 				'</span>'
 			);
 			?>
-		</footer><!-- .entry-footer -->
+        </footer><!-- .entry-footer -->
 	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
