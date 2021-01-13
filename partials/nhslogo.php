42e7ad10c07f47b2f699bc0ce4abed09bf7ae353
Tony Blacker <tblacker7@gmail.com>
2021-01-13 16:22:13 +0000

    Amended logo from role='image' to role='img'

--- partials/nhslogo.php
+++ partials/nhslogo.php
@@ -13,7 +13,7 @@
 
 $header_colour = get_theme_mod( 'header_styles', 'normal' );
 ?>
-<svg class="nhsuk-logo" xmlns="http://www.w3.org/2000/svg" role="image" focusable="false" viewBox="0 0 40 16">
+<svg class="nhsuk-logo" alt="NHS Logo" xmlns="http://www.w3.org/2000/svg" role="img" focusable="false" viewBox="0 0 40 16">
 	<path
 		<?php
 		if ( 'normal' !== $header_colour ) :
