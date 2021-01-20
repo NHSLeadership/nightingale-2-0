ed3678df708326915d6d853964bce8e11ebcadd8
Andrew Blane <andrew.blane@leadershipacademy.nhs.uk>
2021-01-20 10:30:24 +0000

    [refs #DI-2783] Make error message translatable Also use hard-coded filter priority for simplicity.

--- functions.php
+++ functions.php
@@ -485,12 +485,12 @@ function nightingale_check_author( $display_name ) {
     }
     return $display_name;
 }
-add_filter( 'the_author', 'nightingale_check_author', PHP_INT_MAX, 1 );
+add_filter( 'the_author', 'nightingale_check_author', 9999, 1 );
 
 /**
  * For security prevent password reset error message providing info on user emails
  */
 function nightingale_no_login_hints ( $error ) {
-	return 'If your email has been found in our database you will receive a reset link';
+	return _('If your email has been found in our database you will receive a reset link');
 }
 add_filter ( 'login_errors', 'nightingale_no_login_hints' );
