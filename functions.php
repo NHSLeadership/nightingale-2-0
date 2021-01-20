91fa92ecaa92f155291ad1bb3a090f3e2e9e7194
Andrew Blane <andrew.blane@leadershipacademy.nhs.uk>
2021-01-20 14:00:05 +0000

    [refs #DI-2783] Add domain to string translation So that error message translation works.

--- functions.php
+++ functions.php
@@ -491,6 +491,6 @@ add_filter( 'the_author', 'nightingale_check_author', 9999, 1 );
  * For security prevent password reset error message providing info on user emails
  */
 function nightingale_no_login_hints ( $error ) {
-	return __('If your email has been found in our database you will receive a reset link');
+	return __('If your email has been found in our database you will receive a reset link' , 'nightingale');
 }
 add_filter ( 'login_errors', 'nightingale_no_login_hints' );
