d0e973745fae820820e09a1eac7cf67830f33713
Andrew Blane <andrew.blane@leadershipacademy.nhs.uk>
2021-01-21 13:17:14 +0000

    [refs #DI-2796] Reverse link changes Updating title has limited effect on accessibility.

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -65,7 +65,7 @@ else :
 
 				<div class="nhsuk-card__content">
 					<h2 class="nhsuk-card__heading nhsuk-heading-m">
-						<a class="nhsuk-card__link" href="<?php the_permalink(); ?>" title="<?php echo substr(trim(get_the_permalink(),'/'), strripos(trim(get_the_permalink(),'/'),'/')+1); ?>">
+						<a class="nhsuk-card__link" href="<?php the_permalink(); ?>">
 							<?php the_title(); ?>
 						</a>
 					</h2>
