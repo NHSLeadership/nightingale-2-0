26da5500aa9ea300ee1cc3467c1060abaae679ad
Andrew Blane <andrew.blane@leadershipacademy.nhs.uk>
2021-01-22 13:02:48 +0000

    [refs #DI-2766] Make message translatable

--- tribe/events/blocks/event-links.php
+++ tribe/events/blocks/event-links.php
@@ -26,8 +26,8 @@ remove_filter( 'the_content', 'do_blocks', 9 );
 if ( $should_render ) :
 ?>
 	<div class="tribe-block tribe-block__events-link">
-		<h3>Calendar Links</h3>
-		<p>The following links allow you to add this event to your calendar. Note: You need to be logged into your Google account before you can add to your Google calendar.</p>
+		<h3 class="nhsuk-heading-s">Calendar Links</h3>
+		<p> <?php echo __('The following links allow you to add this event to your calendar. Note: You need to be logged into your Google account before you can add to your Google calendar.','nightingale') ?></p>
 		<?php if ( $has_google_cal ) : ?>
 			<div class="tribe-block__btn--link tribe-block__events-gcal">
 				<a
