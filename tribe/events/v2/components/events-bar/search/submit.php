2703fa7affa15cf337109204187e4713b7b19fda
Nick-Summerfield <46444681+Nick-Summerfield@users.noreply.github.com>
2022-02-03 13:45:31 +0000

    Added styling for buttons to improve NHSUK look. JS Loading causing some issues with the display of some buttons

---
+++ tribe/events/v2/components/events-bar/search/submit.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * View: Events Bar Search Submit Input
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/events/v2/components/events-bar/search/submit.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link http://evnt.is/1aiy
+ *
+ * @version 4.9.10
+ *
+ */
+?>
+    <button
+        class="nhsuk-button tribe-events-c-search__button"
+        type="submit"
+        name="submit-bar"
+    >
+        <?php printf( esc_html__( 'Find %s', 'the-events-calendar' ), tribe_get_event_label_plural() ); ?>
+    </button>
+<?php
